65660025da9a1c3904c11b8b753b835a11098edd
Chuxiong Kou <cxk248@student.bham.ac.uk>
2024-08-04 10:26:41 +0100

    1.add complex cnn layer model to cnn

--- CNN.py
+++ CNN.py
@@ -89,6 +89,42 @@ class MiniSimplifyCnnModel(nn.Module):
             return self.target(input_frame)
 
 
+class MiniComplexCnnLayerModel(nn.Module):
+
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        c, h, w = input_dim
+
+        self.online = nn.Sequential(
+            nn.Conv2d(in_channels=c, out_channels=32, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=2, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, output_dim)
+        )
+
+        self.target = copy.deepcopy(self.online)
+
+        for p in self.target.parameters():
+            p.requires_grad = False
+
+    def forward(self, input_frame, model='online'):
+        input_frame = input_frame.to(torch.float)
+        if model == 'online':
+            return self.online(input_frame)
+        elif model == 'target':
+            return self.target(input_frame)
+
+
 class MiniTransformerCnnModel(nn.Module):
 
     def __init__(self, input_dim, output_dim):
